219e763235e49167bed37e71a041ed9cf73c4c01
sakura-lxver <aleanadhiraa@gmail.com>
2024-10-25 17:38:56 -0500

    changed steps to ruby

---
+++ spec/features/step_definitions/AdminEventHome_steps.rb
@@ -0,0 +1,15 @@
+Given('AdminEventHome component is rendered with default properties') do
+    pending
+  end
+  
+  When('the ToggleButtonGroup changes with value {string} and mock response is successful') do |value|
+    pending
+  end
+  
+  When('the ToggleButtonGroup changes with value {string} and mock response fails with status {int}') do |value, status|
+    pending
+  end
+  
+  Then('the rendered component should match the snapshot') do
+    pending
+  end
