c010f4ff4017d53122a2c06b5a96735e91489581
Ritu <ritu251297@gmail.com>
2024-10-28 13:53:41 -0500

    added new spec tests for unused classes

---
+++ spec/channels/channels_spec.rb
@@ -0,0 +1,10 @@
+# spec/channels/channels_spec.rb
+
+require 'rails_helper'
+
+RSpec.describe ApplicationCable::Channel, type: :channel do
+  let(:user) { create(:user) } # Assuming you have a User factory
+
+  # Test for a successful connection
+ 
+end
