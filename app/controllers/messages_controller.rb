46dc181b2d2bfbe01f514dcf6f9775cbe0e4afce
Jose L Salazar <68448647+JSalazar026@users.noreply.github.com>
2024-10-29 17:55:11 -0500

    Made announcements/messages stay within chat window (#67)

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -11,7 +11,7 @@ class MessagesController < ApplicationController
         render json: {comment: "Successfully sent Message as Admin!"}, status: 200
         
       elsif is_user_logged_in?("CLIENT")
-      	create_message_client(params[:event_id], params[:client_id], params[:content], params[:sender], params[receiver])
+      	create_message_client(params[:event_id], params[:client_id], params[:content], params[:sender], params[:receiver])
         render json: {comment: "Successfully sent Message as Client!"}, status: 200
       else
         create_message_talent(params[:event_id], params[:talent_id], params[:content], params[:sender], params[:receiver])
@@ -27,17 +27,17 @@ class MessagesController < ApplicationController
     client = Client.find_by(:_id => clientId)
     
     # UserMailer.added_message(client.email).deliver_now	
-    Message.create(:event_id => eventId, :client_id => clientId, :from => messageSender, :to => messageReceiver, :message => messageContent)
+    Message.create(:event_id => eventId, :user_id => clientId, :from => messageSender, :to => messageReceiver, :message => messageContent)
   end
 
   def create_message_client eventId, clientId, messageContent, messageSender, messageReceiver
-    Message.create(:event_id => eventId, :client_id => clientId, :from => messageSender, :to => messageReceiver, :message => messageContent)
+    Message.create(:event_id => eventId, :user_id => clientId, :from => messageSender, :to => messageReceiver, :message => messageContent)
   end
 
 
-#   def create_message_talent eventId, talentId, messageContent, messageSender, messageReceiver
-#     Message.create(:event_id => eventId, :talent_id => talentId, :from => messageSender, :to => messageReceiver, :message => messageContent)
-#   end
+  def create_message_talent eventId, talentId, messageContent, messageSender, messageReceiver
+    Message.create(:event_id => eventId, :user_id => talentId, :from => messageSender, :to => messageReceiver, :message => messageContent)
+  end
 
 
 end
