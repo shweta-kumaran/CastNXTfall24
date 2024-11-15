7b73c91d2d3d57e350aae2ac9b080451cf8d60b6
Rahul Baid <rahulbaid0110@gmail.com>
2024-11-15 10:42:34 -0600

    remove cucumber-rails

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -15,9 +15,6 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 Object.send(:remove_const, :ActiveRecord)
 
 require 'rspec/rails'
-require 'webdrivers'
-
-Capybara.javascript_driver = :selenium_chrome
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -52,7 +49,7 @@ RSpec.configure do |config|
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
-  config.use_transactional_fixtures = false
+  config.use_transactional_fixtures = true
 
   # You can uncomment this line to turn off ActiveRecord support entirely.
   # config.use_active_record = false
