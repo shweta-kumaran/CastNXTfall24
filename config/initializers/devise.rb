30966ec67511118ecc15f34fcfdafe6d4512096c
Ritu <ritu251297@gmail.com>
2024-11-15 12:17:04 -0600

    remove active record usages

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -273,7 +273,6 @@ Devise.setup do |config|
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
  
-  require 'devise/orm/active_record'
 
   config.omniauth :events360,
   ENV['EVENT360_CLIENT_ID'],
